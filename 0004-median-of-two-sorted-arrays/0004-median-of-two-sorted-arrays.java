bc24d9e86cf9e9e5fb252cc7abc7ec5dfbdbb5bc
deepikabalhara <deepikabalhara00@gmail.com>
2026-05-17 19:08:13 +0530

    Time: 1 ms (100%), Space: 49.1 MB (15.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,37 +1,48 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length-1;
-        int n = nums2.length-1;
-        int[] arr = new int[m+n+2];
-        System.arraycopy(nums1 , 0 , arr , 0 , m+1);
-        System.arraycopy(nums2 , 0 , arr , m+1 , n+1);
-        Arrays.sort(arr);
-        int size = arr.length-1;
-        int div = size /2;
-        if(size%2 ==0)      //odd
-        {
-            return arr[div];
+        int n = nums1.length;
+        int m = nums2.length;
+        int i =0 , j=0 , m1=0 , m2=0;
+        for(int count =0 ; count<=(n+m)/2 ;count++){
+            m2=m1;
+            if(i!=n && j!=m)
+            {
+                if(nums1[i]>nums2[j]){
+                    m1=nums2[j++];
+                }
+                else
+                {
+                    m1=nums1[i++];
+                }
+            }else if(i<n){
+                m1=nums1[i++];
+            }
+            else{
+                m1=nums2[j++];
+            }
+
         }
-        else
-        {
-            float median = (arr[div]+arr[div+1])/2.0f;
-            return median;
+        if((n+m)%2 ==1){
+            return (double) m1;
         }
-
-
-        // for(int i =0;i<=m;i++){
-        //     for(int j =0;j<=n;j++){
-        //         arr[i]=
-        //     }
-        // }
-        // int div = (m+n)/2;
-        // //for even 
-        // if(m<div)
-        // {
-        //     int even = (nums2[div-m]+nums2[div-m+1])/2 ;
-        // }
-        // else{
-
-        // }
-    }
+        else{
+            double ans = (double)m1 + (double)m2;
+            return ans/2.0;
+        }
+    //     int[] arr = new int[m+n+2];
+    //     System.arraycopy(nums1 , 0 , arr , 0 , m+1);
+    //     System.arraycopy(nums2 , 0 , arr , m+1 , n+1);
+    //     Arrays.sort(arr);
+    //     int size = arr.length-1;
+    //     int div = size /2;
+    //     if(size%2 ==0)      //odd
+    //     {
+    //         return arr[div];
+    //     }
+    //     else
+    //     {
+    //         float median = (arr[div]+arr[div+1])/2.0f;
+    //         return median;
+    //     }
+     }
 }
